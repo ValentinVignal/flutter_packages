50c65d63645ac3a509a9270241e53f855a0647b7
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-09-13 22:53:52 +0800

    :white_check_mark: Update the tests

---
+++ packages/animated_collection/test/widgets/animated_visibility_test.dart
@@ -0,0 +1,89 @@
+import 'package:animated_collection/animated_collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'It should build the child when visible is true',
+    (tester) async {
+      const key = Key('key');
+      await tester.pumpWidget(
+        const Directionality(
+          textDirection: TextDirection.ltr,
+          child: AnimatedVisibility(
+            visible: true,
+            child: SizedBox(key: key),
+          ),
+        ),
+      );
+
+      expect(find.byKey(key), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'It should no build the child when visible is false',
+    (tester) async {
+      const key = Key('key');
+      await tester.pumpWidget(
+        const Directionality(
+          textDirection: TextDirection.ltr,
+          child: AnimatedVisibility(
+            visible: false,
+            child: SizedBox(key: key),
+          ),
+        ),
+      );
+
+      expect(find.byKey(key), findsNothing);
+    },
+  );
+
+  testWidgets(
+    'It should show and hide the child when visible changes',
+    (tester) async {
+      const key = Key('key');
+      Future<void> pumpWidget(bool visible) async {
+        final widget = Directionality(
+          textDirection: TextDirection.ltr,
+          child: AnimatedVisibility(
+            visible: visible,
+            curve: Curves.linear,
+            child: const ColoredBox(
+              color: Colors.red,
+              child: SizedBox.square(
+                dimension: 50,
+                key: key,
+              ),
+            ),
+          ),
+        );
+        await tester.pumpWidget(widget);
+      }
+
+      await pumpWidget(true);
+
+      final actualPositionedBox = tester.renderObject<RenderPositionedBox>(
+        find.byType(Align),
+      );
+      expect(find.byKey(key), findsOneWidget);
+      expect(actualPositionedBox.heightFactor, 1);
+      expect(actualPositionedBox.widthFactor, 1);
+
+      await pumpWidget(false);
+      await tester.pump(AnimatedVisibility.defaultDuration * 0.5);
+      expect(find.byKey(key), findsOneWidget);
+      expect(actualPositionedBox.heightFactor, 0.5);
+      expect(actualPositionedBox.widthFactor, 1);
+
+      await tester.pump(AnimatedVisibility.defaultDuration * 0.5);
+      expect(find.byKey(key), findsOneWidget);
+      expect(actualPositionedBox.heightFactor, 0.0);
+      expect(actualPositionedBox.widthFactor, 1);
+
+      await tester.pumpAndSettle();
+      expect(find.byKey(key), findsNothing);
+    },
+  );
+}
