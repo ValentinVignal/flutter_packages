65536651e2109c0713bcfc36c7e6e6a74db3409c
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-09-13 23:08:12 +0800

    :fire: Remove useless AnimatedHorizontalSlide

--- packages/animated_collection/lib/animated_collection.dart
+++ packages/animated_collection/lib/animated_collection.dart
@@ -2,7 +2,6 @@ library animated_collection;
 
 export 'src/ticker_future.dart';
 export 'src/widgets/animated_boolean_state.dart';
-export 'src/widgets/animated_horizontal_slide.dart';
 export 'src/widgets/animated_location.dart';
 export 'src/widgets/animated_size_transition.dart';
 export 'src/widgets/animated_visibility.dart';
