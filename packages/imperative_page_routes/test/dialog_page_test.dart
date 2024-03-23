71f83285aa1a4b5f6adb9d8f72aaeda291649ad2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-03-23 17:21:16 +0800

    :white_check_mark: Add tests

---
+++ packages/imperative_page_routes/test/dialog_page_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:imperative_page_routes/imperative_page_routes.dart';
+
+void main() {
+  testWidgets("It should push a dialog page", (tester) async {
+    final pages = [
+      const MaterialPage(child: Text('Home')),
+      const DialogPage(child: Text('Dialog')),
+    ];
+    final navigator = GlobalKey<NavigatorState>();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Navigator(
+          key: navigator,
+          pages: pages,
+          onPopPage: (route, result) {
+            pages.removeLast();
+            return route.didPop(result);
+          },
+        ),
+      ),
+    );
+    expect(find.text('Home'), findsOne);
+    expect(find.text('Dialog'), findsOne);
+
+    navigator.currentState!.pop();
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('Home'), findsOne);
+    expect(find.text('Dialog'), findsNothing);
+    expect(pages, hasLength(1));
+  });
+}
---
+++ packages/imperative_page_routes/test/drawer_page_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:imperative_page_routes/imperative_page_routes.dart';
+
+void main() {
+  testWidgets("It should push a dialog page", (tester) async {
+    final pages = [
+      const MaterialPage(child: Text('Home')),
+      const DrawerPage(child: Text('Drawer')),
+    ];
+    final navigator = GlobalKey<NavigatorState>();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Navigator(
+          key: navigator,
+          pages: pages,
+          onPopPage: (route, result) {
+            pages.removeLast();
+            return route.didPop(result);
+          },
+        ),
+      ),
+    );
+    expect(find.text('Home'), findsOne);
+    expect(find.text('Drawer'), findsOne);
+
+    navigator.currentState!.pop();
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('Home'), findsOne);
+    expect(find.text('Drawer'), findsNothing);
+    expect(pages, hasLength(1));
+  });
+}
