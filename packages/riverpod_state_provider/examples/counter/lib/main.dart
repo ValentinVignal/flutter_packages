907efe0e679e0c73db35f8a188b11f14ea750709
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-12-20 20:07:39 +0800

    :memo: Clean example

--- packages/riverpod_state_provider/examples/counter/lib/main.dart
+++ packages/riverpod_state_provider/examples/counter/lib/main.dart
@@ -5,18 +5,10 @@ import 'package:riverpod_state_provider_annotation/riverpod_state_provider_annot
 part 'main.g.dart';
 part 'main.rsp.dart';
 
-// A Counter example implemented with riverpod
-
 void main() {
   runApp(
-    // Adding ProviderScope enables Riverpod for the entire project
     ProviderScope(
-      child: ProviderScope(
-        overrides: [
-          myIntegerProvider.overrideWithValue(42),
-        ],
-        child: const MyApp(),
-      ),
+      child: const MyApp(),
     ),
   );
 }
@@ -35,42 +27,6 @@ int myInteger() {
   return 0;
 }
 
-@RiverpodStateProvider()
-int? myNullableInteger() {
-  return 0;
-}
-
-@RiverpodStateProvider()
-double myDouble() {
-  return 0;
-}
-
-class MyClass<T> {
-  const MyClass({required this.value});
-
-  final T value;
-}
-
-@RiverpodStateProvider()
-MyClass<int> myClassInteger() {
-  return MyClass(value: 0);
-}
-
-@RiverpodStateProvider()
-Set<int> mySetInteger() {
-  return {0};
-}
-
-@RiverpodStateProvider()
-List<int> myListInteger() {
-  return [0];
-}
-
-@RiverpodStateProvider()
-Map<int, int> myMapInteger() {
-  return {0: 0};
-}
-
 @riverpod
 class Counter extends _$Counter {
   /// Classes annotated by `@riverpod` **must** define a [build] function.
@@ -79,18 +35,6 @@ class Counter extends _$Counter {
   /// You can also freely define parameters on this method.
   @override
   int build() => 0;
-
-  void increment() => state++;
-}
-
-@riverpod
-class CounterFamily extends _$CounterFamily {
-  /// Classes annotated by `@riverpod` **must** define a [build] function.
-  /// This function is expected to return the initial state of your shared state.
-  /// It is totally acceptable for this function to return a [Future] or [Stream] if you need to.
-  /// You can also freely define parameters on this method.
-  @override
-  int build(String param) => 0;
 
   void increment() => state++;
 }
@@ -108,12 +52,6 @@ class Home extends ConsumerWidget {
           [
             '${ref.watch(counterProvider)}',
             '${ref.watch(myIntegerProvider)}',
-            '${ref.watch(myNullableIntegerProvider)}',
-            '${ref.watch(myDoubleProvider)}',
-            '${ref.watch(myClassIntegerProvider).value}',
-            '${ref.watch(mySetIntegerProvider)}',
-            '${ref.watch(myListIntegerProvider)}',
-            '${ref.watch(myMapIntegerProvider)}',
           ].join('\n'),
         ),
       ),
@@ -122,22 +60,6 @@ class Home extends ConsumerWidget {
         onPressed: () {
           ref.read(counterProvider.notifier).increment();
           ref.read(myIntegerProvider.notifier).state++;
-          ref
-              .read(myNullableIntegerProvider.notifier)
-              .update((state) => (state ?? 0) + 1);
-          ref.read(myDoubleProvider.notifier).state += 0.5;
-          ref
-              .read(myClassIntegerProvider.notifier)
-              .update((state) => MyClass(value: state.value + 1));
-          ref
-              .read(mySetIntegerProvider.notifier)
-              .update((state) => {state.single + 1});
-          ref
-              .read(myListIntegerProvider.notifier)
-              .update((state) => [state.single + 1]);
-          ref
-              .read(myMapIntegerProvider.notifier)
-              .update((state) => {0: state[0]! + 1});
         },
         child: const Icon(Icons.add),
       ),
