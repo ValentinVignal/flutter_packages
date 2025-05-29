1260cc5cee32591d07572f8feb7fc8985bce659e
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-05-29 15:38:18 +0800

    Add example

---
+++ packages/static_json_serializable/examples/color_palette_example/test/ensure_build_test.dart
@@ -0,0 +1,12 @@
+import 'package:build_verify/build_verify.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'Ensure build',
+    () async => expectBuildClean(
+      packageRelativeDirectory:
+          'packages/static_json_serializable/examples/color_palette_example',
+    ),
+  );
+}
