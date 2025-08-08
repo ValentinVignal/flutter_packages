ae04e609439a3f76972c036fc0332ca500681def
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-08-08 12:13:43 +0800

    Add test

--- packages/animated_collection/test/flutter_test_config.dart
+++ packages/animated_collection/test/flutter_test_config.dart
@@ -5,6 +5,5 @@ import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   LeakTesting.enable();
   LeakTracking.warnForUnsupportedPlatforms = false;
-  LeakTesting.settings = LeakTesting.settings.withCreationStackTrace();
   await testMain();
 }
