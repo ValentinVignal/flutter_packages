a90eb053242e559fb4b3bbc4cb037bd07051c4b2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-08-18 11:27:53 +0800

    Update the tests

--- packages/static_json_serializable/static_json_serializable_generator/test/integration/integration.g.dart
+++ packages/static_json_serializable/static_json_serializable_generator/test/integration/integration.g.dart
@@ -6,7 +6,9 @@ part of 'integration.dart';
 // StaticJsonSerializableGenerator
 // **************************************************************************
 
-const $intClassStaticJson = {
+final $intClassStaticJson = {
   'staticConstValue1': IntClass.staticConstValue1,
   'staticConstValue2': IntClass.staticConstValue2,
+  'staticFinalValue1': IntClass.staticFinalValue1,
+  'staticFinalValue2': IntClass.staticFinalValue2,
 };
