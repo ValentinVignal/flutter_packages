04c056adb3c8b8ee116784721f810a17ab752157
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-02-28 17:06:42 +0800

    Update examples

--- packages/riverpod_state_provider/examples/counter/lib/main.g.dart
+++ packages/riverpod_state_provider/examples/counter/lib/main.g.dart
@@ -6,36 +6,111 @@ part of 'main.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(Counter)
+final counterProvider = CounterProvider._();
+
+final class CounterProvider extends $NotifierProvider<Counter, int> {
+  CounterProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'counterProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$counterHash();
+
+  @$internal
+  @override
+  Counter create() => Counter();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(int value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<int>(value),
+    );
+  }
+}
+
 String _$counterHash() => r'4243b34530f53accfd9014a9f0e316fe304ada3e';
 
-/// See also [Counter].
-@ProviderFor(Counter)
-final counterProvider = AutoDisposeNotifierProvider<Counter, int>.internal(
-  Counter.new,
-  name: r'counterProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$counterHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+abstract class _$Counter extends $Notifier<int> {
+  int build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<int, int>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<int, int>,
+              int,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
 
-typedef _$Counter = AutoDisposeNotifier<int>;
-String _$myIntegerStateHash() => r'6c2b5cfbbe99e6c419179416e0967e9e42cf1e1f';
-
-/// See also [MyIntegerState].
 @ProviderFor(MyIntegerState)
-final myIntegerStateProvider =
-    AutoDisposeNotifierProvider<MyIntegerState, int>.internal(
-      MyIntegerState.new,
-      name: r'myIntegerStateProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$myIntegerStateHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
+@StateProviderFor(myInteger)
+final myIntegerStateProvider = MyIntegerStateProvider._();
 
-typedef _$MyIntegerState = AutoDisposeNotifier<int>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+@StateProviderFor(myInteger)
+final class MyIntegerStateProvider
+    extends $NotifierProvider<MyIntegerState, int> {
+  MyIntegerStateProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'myIntegerStateProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$myIntegerStateHash();
+
+  @$internal
+  @override
+  MyIntegerState create() => MyIntegerState();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(int value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<int>(value),
+    );
+  }
+}
+
+String _$myIntegerStateHash() => r'95af481ddad2d13ef47aac9e77381d34803f145c';
+
+@StateProviderFor(myInteger)
+abstract class _$MyIntegerState extends $Notifier<int> {
+  int build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<int, int>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<int, int>,
+              int,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
